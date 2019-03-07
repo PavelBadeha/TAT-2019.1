3e2c2a4e3de40686a9ba89571ba16a293b7a5edb
Pavel Badeha <pbadzelha@mail.ru>
2019-03-07 18:03:17 +0300

    DEV-1 v.1.1

--- DEV-1/DEV-1/Program.cs
+++ DEV-1/DEV-1/Program.cs
@@ -9,41 +9,39 @@ namespace DEV_1
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < args.Length; i++)
+            for (int i = 0; i < args.Length; i++)          //Провека на правильность ввода 
             {
                 string slovo = args[i];
                 if (slovo.Length < 2)
                 {
-                    Console.WriteLine("Empty or less then 2 symbols");
+                    Console.WriteLine("Empty or less than 2 symbols");
                 }
-
                 Console.WriteLine($"\n{slovo}\n");
                 SubStringFinder obj = new SubStringFinder(slovo);
             }
             Console.ReadKey();
         }
     }
-    class SubStringFinder
+    class SubStringFinder                   //Класс который ищет подстроки 
     {
-        public SubStringFinder(string slovo)
+        public SubStringFinder(string slovo)  //Конструктор принимающий строку 
         {
             bool flag = false;
-            string buff;
-            string buff2 = string.Empty;
+            string buff;                   // Переменная buff будет принимать в себя подстроки
+            string buff2 = string.Empty;   //Перенменная buff2 будет принимать в себя подстроки подстроки(buff) 
             buff = string.Concat(slovo[0]);
             for (int i = 1; i < slovo.Length; i++)
             {
                 if (slovo[i] != slovo[i - 1])
                 {
-                    buff += slovo[i];
+                    buff += slovo[i];              //Поиск подстрок с последовательно неповторяющимеся символами 
                 }
                 else
                 {
                     flag = true;
                 }
-
-                if (flag || (!flag && i == slovo.Length - 1))
-                    while (buff.Length >= 2)
+                if (flag || ( i == slovo.Length - 1))  //Вычлинение из подстроки все возможные подстроки 
+                    while (buff.Length >= 2)                   //Более 2-ух символов 
                     {
                         for (int k = 0; k < buff.Length; k++)
                         {
