0c630970cf0c2845f1214132cc2572a5831c98da
Pavel Badeha <pbadzelha@mail.ru>
2019-03-12 19:07:31 +0300

    Dev-1 v.3

---
+++ DEV-1 v.2/DEV-1 v.2/EntryPoint.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DEV_1
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            int ErrorIndex = 0;//Index of the word from the command line where the error occurred
+            try
+            {
+                if (args.Length == 0)
+                {
+                    throw new ArgumentNullException();
+                }
+                for (int i = 0; i < args.Length; i++)
+                {
+                    string str = args[i];
+                    if (str.Length < 2)
+                    {
+                        ErrorIndex = i;
+                        throw new FormatException();
+                    }
+
+                    FinderUniqueSubString UniqueSequence = new FinderUniqueSubString(str);
+                    UniqueSequence.SearchSubString();
+                    UniqueSequence.PrintUniqueSequence();
+                }
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"Word caused format exception: {args[ErrorIndex]}");
+                Console.WriteLine("Less than 2 symbols");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Empty string");
+            }
+        }   
+    }
+}
---
+++ DEV-1 v.2/DEV-1 v.2/FinderUniqueSubString.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+
+namespace DEV_1
+{
+    public class FinderUniqueSubString 
+    {
+        /// <summary>
+        /// <param name="str"></param> string that is subject to change
+        /// <param name="BuffResultSting"></param>buffer StringBuilder that will be contain all unique subsequences
+        /// <param name="ResultString"></param> Result string which contain all unique subsequences
+        /// </summary>
+       
+        private string str = string.Empty;
+        private StringBuilder BuffResultString = new StringBuilder(string.Empty);
+        private string ResultString = string.Empty;
+
+        public FinderUniqueSubString(string _str)
+        {
+            str = _str;
+        }
+        /// <summary>
+        /// Method that SearchSubString
+        /// </summary>
+        public void SearchSubString()
+        {
+            /// <summary>
+            ///<param name="flag"></param> variable that monitors whether the loop has reached repeated letters
+            ///<param name="buff"></param> buffer variable that contain substring
+            /// <param name="buff2"></param> buffer variable that contain substring of substring 
+            /// </summary>
+            bool flag = false;
+            string buff = string.Concat(str[0]);
+            string buff2 = string.Empty;
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (str[i] != str[i - 1])
+                {
+                    buff += str[i];             
+                }
+                else
+                {
+                    flag = true;
+                }
+                if (flag || (i == str.Length - 1))  
+                    while (buff.Length >= 2)                  
+                    {
+                        for (int k = 0; k < buff.Length; k++)
+                        {
+                            buff2 += buff[k];
+                            if (buff2.Length >= 2)
+                            {
+                               BuffResultString.Append($"{buff2}\n");
+                            }
+                        }
+                        buff = buff.TrimStart(buff[0]);
+                        buff2 = string.Empty;
+                    }
+                flag = false;
+            }
+            ResultString = BuffResultString.ToString();
+        }
+        /// <summary>
+        /// Method that displays Result string
+        /// </summary>
+        public void PrintUniqueSequence()
+        {
+            Console.WriteLine($"Source word: {str}");
+            Console.WriteLine(ResultString);
+        }
+    }
+}
