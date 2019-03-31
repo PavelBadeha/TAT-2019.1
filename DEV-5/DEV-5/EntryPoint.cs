05ed893039452544a8724c4893114f571def5536
Pavel Badeha <pbadzelha@mail.ru>
2019-03-31 17:21:29 +0300

    DEV-5

---
+++ DEV-5/DEV-5/EntryPoint.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DEV_5
+{
+    /// <summary>
+    /// The entry point.
+    /// Create flying object
+    /// </summary>
+    class EntryPoint
+    {
+        /// <summary>
+        /// The main method colculate flying time from start point to finish point
+        /// </summary>
+        static void Main()
+        {
+            try
+            {
+                Point startPoint = new Point(0, 0, 0);
+                Point finishPoint = new Point(100, 200, 800);
+                List<FlyObject> flyObjects = new List<FlyObject>();
+                flyObjects.Add(new Bird(startPoint));
+                flyObjects.Add(new Plane(startPoint));
+                flyObjects.Add(new SpaceShip(startPoint));
+                foreach (var flyObj in flyObjects)
+                {
+                    flyObj.FlyTo(finishPoint);
+                    flyObj.DisplayInfo();
+                }
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("ERROR!");
+            }          
+        }
+    }
+}
---
+++ DEV-5/DEV-5/IFlyable.cs
@@ -0,0 +1,28 @@
+﻿
+namespace DEV_5
+{
+    /// <summary>
+    /// Interface for flying objects.
+    /// </summary>
+    interface IFlyable
+    {
+        /// <summary>
+        ///The method that returns the distance between start and finish points
+        /// </summary>
+        /// <param name="newPoint">finish point</param>
+        /// <returns></returns>
+        double FlyTo(Point newPoint);
+
+        /// <summary>
+        /// The method that returns object name
+        /// </summary>
+        /// <returns></returns>
+        object WhoAmI();
+
+        /// <summary>
+        ///The method that returns flight time
+        /// </summary>
+        /// <returns></returns>
+        double GetFlyTime();
+    }
+}
